dbc34c1646a10d7d93d6e4cae4a4b21f4bee7ce1
trustworthyaxolotl26 <efreeman2029@kehillahstudent.org>
2026-05-05 10:17:42 -0700

    new game. still in proposal.

--- flip.py
+++ flip.py
@@ -12,7 +12,9 @@ BLUE  = (0, 0, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
+NAVY = (0, 0, 70)
 
 # Screen information
 SCREEN_WIDTH = 600
@@ -22,15 +24,17 @@ SCORE = 0
 #score = coins
 
 #background
-background = pygame.image.load("Background.png")
+start = pygame.image.load("start.png")
+background = pygame.image.load("floor.png")
+finish = pygame.image.load("end.png")
 
 #fonts or something idk
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
 
-DISPLAYSURF = pygame.display.set_mode((600,600))
-DISPLAYSURF.fill(WHITE)
+DISPLAYSURF = pygame.display.set_mode((1470,600))
+DISPLAYSURF.fill(NAVY)
 pygame.display.set_caption("Game")
  
  
@@ -54,49 +58,29 @@ class Player(pygame.sprite.Sprite):
         super().__init__() 
         self.image = pygame.image.load("Player.png")
         self.rect = self.image.get_rect()
+        # self.rect = pygame.Rect.normalize()
         self.rect.center = (160, 520)
- 
-    def move(self):
-        pressed_keys = pygame.key.get_pressed()
-        if self.rect.height < SCREEN_HEIGHT:
-            if pressed_keys[K_UP] or pressed_keys[K_w]:
-                self.rect.move_ip(0, -5)
-        if self.rect.height > SCREEN_HEIGHT:
-            if pressed_keys[K_DOWN] or pressed_keys[K_s]:
-                self.rect.move_ip(0,5)
-        if self.rect.left > 0:
-              if pressed_keys[K_LEFT] or pressed_keys[K_a]:
-                  self.rect.move_ip(-5, 0)
-        if self.rect.right < SCREEN_WIDTH:        
-              if pressed_keys[K_RIGHT] or pressed_keys[K_d]:
-                  self.rect.move_ip(5, 0)
 
-milkshakes = ["strawberry", "vanilla", "mixed"]
-class Milkshake(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.order = random.choice(milkshakes)
-        self.image = pygame.image.load(f"{self.order}.png")
-        self.rect = self.image.get_rect()
-    
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[K_SPACE]:
-            self.rect == Player.rect
+        if pressed_keys[K_UP] or pressed_keys[K_w]:
+            self.rect = (0, 100)
+        if pressed_keys[K_DOWN] or pressed_keys[K_s]:
+            self.rect = (0, 200)
+        if pressed_keys[K_LEFT] or pressed_keys[K_a]:
+            self.rect.move_ip(-5, 0)   
+        if pressed_keys[K_RIGHT] or pressed_keys[K_d]:
+            self.rect.move_ip(5, 0)
 
 P1 = Player()
 # E1 = Enemy()
-M1 = Milkshake()
 
 ##Creating Sprites Groups
 # enemies = pygame.sprite.Group()
 # enemies.add(E1)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
-all_sprites.add(M1)
 # all_sprites.add(E1)
-foods = pygame.sprite.Group()
-foods.add(M1)
 
 #Adding a new User event 
 INC_SPEED = pygame.USEREVENT + 1
@@ -111,7 +95,9 @@ while True:
             pygame.quit()
             sys.exit()
      
-    DISPLAYSURF.blit(background, (0,0))
+    DISPLAYSURF.blit(background, (1000,0))
+    DISPLAYSURF.blit(start, (-200,0))
+    DISPLAYSURF.blit(finish, (3000,0))
     # scores = font_small.render(str(SCORE), True, BLACK)
     # DISPLAYSURF.blit(scores, (10,10))
 
--- proposal.md
+++ proposal.md
@@ -1,7 +1,7 @@
 1. milkshake mixer
 a game where you run a shop and make milkshakes and sell them and go on missions to get ingredients and you make money to upgrade stuff and expand your shop and hire workers and unlock different types of milkshakes. customers could also ask for togo or for here or something. 
 - pick up milkshakes
-- get costomers 
+- get customers 
 - collect money
 - make sprites
 
@@ -9,7 +9,7 @@ a game where you run a shop and make milkshakes and sell them and go on missions
 a more simple platformer where there are 2 sides and you can flip between either side to get to the end. levels are randomly generated.  
 - make a level
 - make collison
-- make a good gravity thing for jumping 
+- be able to flip
 
 3. cat collecter
 a game where you collect cats and can play with them or feed them or something idk
